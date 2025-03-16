1a1a8050c15464b3f68cee42029d11d42b0b1ba8
dddayo <alan0960214@gmail.com>
2025-03-16 17:04:12 +0800

    update

---
+++ App.js
@@ -0,0 +1,13 @@
+import Game from "./components/Game";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>文字冒險遊戲</h1>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
